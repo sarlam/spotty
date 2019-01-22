dcae0f98e41504ce4015a8c441e3ee6c4357d99b
Paul Jarleton <paul@jarleton.me>
2019-01-22 19:00:43 +0100

    fix path to file on coverage

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,16 @@ const path = require('path')
 
 module.exports = {
   chainWebpack: config => {
-    const IAddIstanbul = process.env.NODE_ENV === "test"
-      || (process.env.NODE_ENV === "development" && process.env.VUE_APP_TARGET_ENV === 'web')
+    const IAddIstanbul = process.env.NODE_ENV === "test";
 
     if (IAddIstanbul) {
       config.devtool('eval')
       config.module
         .rule('istanbul')
         .test(/\.(js|vue)$/)
-        .enforce('post')
+        .post()
         .include
-        .add(path.resolve(__dirname, '/src'))
+        .add(path.resolve(__dirname, '/src/**/*'))
         .end()
         .use('istanbul-instrumenter-loader')
         .loader('istanbul-instrumenter-loader')
