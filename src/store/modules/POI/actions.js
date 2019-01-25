f907a3c44fc7b8855fdec801085edabafdc8c595
Paul Jarleton <paul@jarleton.me>
2019-01-25 16:29:50 +0100

    add a POI to the store

--- src/store/modules/POI/actions.js
+++ src/store/modules/POI/actions.js
@@ -1,5 +1,30 @@
+import _ from 'lodash'
+
+const ID_PREFIX = 'poi_'
+
 /**
  * PLEASE REMEMBER THAT ACTIONS WILL BE EXECUTED BY THE MAIN PROCESS OF ELECTRON
  */
 export default {
+
+  /**
+   *
+   * @param commit
+   * @param {Object} item
+   * @param {String} item.name
+   * @param {{x: Number, y: Number}} item.pos
+   */
+  add ({ commit }, item) {
+    const { name, pos } = item
+
+    if (!_.isEmpty(name) && !_.isUndefined(pos)) {
+      const storedItem = {
+        _id: _.uniqueId(ID_PREFIX),
+        name,
+        pos
+      }
+
+      commit('ADD_A_POI', storedItem)
+    }
+  }
 }
