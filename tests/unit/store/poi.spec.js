0e2bd5e93f239a90654e6657a2da25ca50e8bcff
Paul Jarleton <paul@jarleton.me>
2019-01-25 16:02:09 +0100

    basic poi module spec (test should fail)

--- tests/unit/store/poi.spec.js
+++ tests/unit/store/poi.spec.js
@@ -7,5 +7,24 @@ import store from '@store'
 describe('POI store module', () => {
   it('should have loaded POI Module store', () => {
     expect(store.state.poi).to.be.an.instanceOf(Object)
+    expect(store.state.poi.list).to.be.an.instanceOf(Array)
+    expect(store.state.poi.list).to.have.length(0)
+  })
+
+  it('Should push a new POI in the store', () => {
+    const poiToAdd = {
+      name: 'dummypoint',
+      pos: {
+        x: 100,
+        y: 50
+      }
+    }
+
+    store.dispatch('poi/add', poiToAdd)
+
+    expect(store.state.poi.list).to.have.length(1)
+    expect(store.state.poi.list[0]._id).to.exist
+    expect(store.state.poi.list[0].name).to.equal(poiToAdd.name)
+    expect(store.state.poi.list[0].pos).to.deep.equal(poiToAdd.pos)
   })
 })
