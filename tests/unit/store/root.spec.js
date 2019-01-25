1382f5783d680974d135e8c904f57d44017c9ac7
Paul Jarleton <paul@jarleton.me>
2019-01-25 15:51:17 +0100

    add store unit tests

---
+++ tests/unit/store/root.spec.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai'
+
+import store from '@store'
+
+/* eslint-disable no-unused-expressions */ // expect(...).to.be.true
+
+describe('root store test', () => {
+  /**
+   * As This soft was design to be used with vuex-electron primarily we test mostly actions
+   */
+
+  it('Should set modal', () => {
+    expect(store.state.aboutModal).to.be.false
+
+    store.dispatch('setAboutModal', true)
+
+    expect(store.state.aboutModal).to.be.true
+  })
+
+  describe('Stage Size', () => {
+    it('Should set a stage size of 2000*1000', () => {
+      expect(store.state.stage).to.deep.equal({ width: 0, height: 0 })
+
+      store.dispatch('setStageSize', { width: 2000, height: 1000 })
+
+      expect(store.state.stage).to.deep.equal({ width: 2000, height: 1000 })
+    })
+
+    // TODO : loading getter using map and stage size to know
+    // TODO : prevent from mutations with negative stage size
+  })
+})
